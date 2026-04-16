fcb37838ce8421c81df466032442021cb187ca34
Demergxo <javiergmpgxo@gmail.com>
2026-04-16 15:44:33 +0200

    finalizado sin probar

--- 01. MAHOU/Stock/descarga_edi.py
+++ 01. MAHOU/Stock/descarga_edi.py
@@ -1,10 +1,15 @@
 import os
-import requests
+import re
 from urllib.parse import quote
+import base64
+
 import pandas as pd
+import requests
 import openpyxl
 from sqlalchemy import create_engine, text
-import re
+from datetime import datetime
+
+RAW_DIR = "raw_files"
 
 def leer_excel(ruta_archivo, hoja=0):
     """
@@ -14,29 +19,50 @@ def leer_excel(ruta_archivo, hoja=0):
         #leemos solo la columna A
         df = pd.read_excel(ruta_archivo, engine='openpyxl', sheet_name=hoja, usecols="A")
         
-        #Eliminamos filas vacias
-        df = df.dropna()
-        #Resetear índice
-        df = df.reset_index(drop=True)
-        
+        #Eliminamos filas vacias y resetear índice
+        df = df.dropna().reset_index(drop=True)
+                
         return df
     except Exception as e:
         print(f"Error al leer el archivo {ruta_archivo}: {e}")
         return None 
+    
+def obtener_placeholders(lista, prefijo="id"):
+    """
+    Genera una lista de placeholders para una consulta SQL.
+    """
+    placeholders = ",".join([f":{prefijo}{i}" for i in range(len(lista))])
+    params = {f"{prefijo}{i}": v for i, v in enumerate(lista)}
+    
+    return placeholders, params
+
+def generar_nombres_unicos(filepath):
+    base, ext = os.path.splitext(filepath)
+    contador = 1
+
+    while os.path.exists(filepath):
+        filepath = f"{base}_{contador}{ext}"
+        contador += 1
+    return filepath
 
 def generar_df_trabajo():
 
     engine = create_engine("mssql+pyodbc://@XGA_PROD")
     #Leer archivo excel
-    df = leer_excel(r"Z:\Shared\Logistica\01. MAHOU\Stock\Listado de pedidos Mahou.xlsx", 1)
+   
+    df = leer_excel(r"C:\Users\jgmeras\OneDrive - GXO\Escritorio\Archivo_Subida_albaranes.xlsx", 1)
+
+    if df is None or df.empty:
+        print("El archivo está vacío o no se pudo leer.")
+        return pd.DataFrame()  # Devuelve un DataFrame vacío en caso de error
 
     #Extraer valores únicos de la columna A
     valores = df.iloc[:, 0].dropna().astype(str).unique().tolist() #type:ignore
-
+    
     if not valores:
         return pd.DataFrame()  # evita query inválida tipo IN ()
 
-    placeholders = ", ".join([f":id{i}" for i in range(len(valores))])
+    placeholders, params = obtener_placeholders(valores)
 
     query_iddoc = text(f"""
         SELECT
@@ -50,11 +76,14 @@ def generar_df_trabajo():
             AND CodigoTipoDocumento = 'ALB'
             AND AlbaranDoc IN ({placeholders})
     """)
-    params = {f"id{i}": v for i, v in enumerate(valores)}
-    
+
     with engine.connect() as conn:
         df_2 = pd.read_sql(query_iddoc, conn, params=params)
     print(df_2)
+
+    if df_2.empty:
+        print("No se encontraron registros.")
+        return pd.DataFrame()
     
     # Extraer los ID_Doc como lista
     ids_alb = (
@@ -68,11 +97,11 @@ def generar_df_trabajo():
     )
     print(ids_alb)
 
-    placeholders = ", ".join([f":id{i}" for i in range(len(ids_alb))])
+    placeholders, params = obtener_placeholders(ids_alb)
 
     query_subestados = text(f"""
         SELECT
-            ID_Doc AS [ID_Doc]
+            ID_Doc AS [ID_Doc],
             ID_SubEstadosDocumentos AS [ID_SubEstadosDocumentos]
         FROM
             SubEstadosDocumentos
@@ -80,8 +109,7 @@ def generar_df_trabajo():
             ID_Cliente = 944
             AND ID_Doc IN ({placeholders})
     """)
-    params = {f"id{i}": v for i, v in enumerate(ids_alb)}
-
+    
     with engine.connect() as conn:
         df_3 = pd.read_sql(query_subestados, conn, params=params)
     print(df_3)
@@ -91,18 +119,18 @@ def generar_df_trabajo():
     id_subest =df_23["ID_SubEstadosDocumentos"].dropna().astype(str).unique().tolist()
     print(id_subest)
 
-    placeholders = ", ".join([f":id{i}" for i in range(len(id_subest))])
+    placeholders, params = obtener_placeholders(id_subest)
 
     query_ficheros = text(f"""
         SELECT
-            ID_SubEstadosDocumentos AS [ID_SubEstadosDocumentos]
+            ID_SubEstadosDocumentos AS [ID_SubEstadosDocumentos],
             NombreFicheroBackupSubEstadoTransmision
         FROM
             SubEstadosTransmision
         WHERE
             ID_SubEstadosDocumentos IN ({placeholders})
     """)
-    params = {f"id{i}": v for i, v in enumerate(id_subest)}
+    
     with engine.connect() as conn:
         df_4 = pd.read_sql(query_ficheros, conn, params=params)
     print(df_4)
@@ -110,8 +138,20 @@ def generar_df_trabajo():
     df_234 =df_23.merge(df_4, on="ID_SubEstadosDocumentos", how="inner")
     print(df_234)
     df_234.to_csv("prueba.csv", index=False, sep=";", encoding="utf-8")
-    id_resul =df_234["NombreFicheroBackupSubEstadoTransmision"].dropna().astype(str).unique().tolist()
-    return id_resul
+    rutas =df_234["NombreFicheroBackupSubEstadoTransmision"].dropna().astype(str).unique().tolist()
+    return rutas
+
+def extraer_nombre_fichero(ruta, content_disposition='"'):
+    if "filename=" in content_disposition:
+        raw_filename = content_disposition.split("filename=")[-1].strip().strip('"')
+        return os.path.basename(raw_filename).replace("\\", "_")
+    
+    nombre = os.path.basename(ruta.replace("\\", "/"))
+    if nombre:
+        return nombre
+    
+    m = re.search(r"I1084[^\\]+$", ruta)
+    return m.group(0) if m else "fichero_descagado.edi"
 
 def descargar_edi():
     user = "JGMERAS"
@@ -119,80 +159,165 @@ def descargar_edi():
 
     base_url = "http://10.19.16.125"
     download_path = "/fga/MtoDocumentosTr/DescargaFicheroSubestado"
+    os.makedirs(RAW_DIR, exist_ok=True)
     ruta_list = generar_df_trabajo()
+    if not ruta_list: #type:ignore
+        print("No hay rutas para procesar.")
+        return
+    
+    session = requests.Session()
+    session.headers.update({
+        "User-Agent": "Mozilla/5.0"
+    })
 
-    for ruta in ruta_list:
+    try:
+        #hacemos login con la primera opción
+        primera_ruta = ruta_list[0]
+        primera_url = f"{base_url}{download_path}?ruta={quote(primera_ruta)}"#type:ignore
 
-        # Codificar correctamente
-        ruta_codificada = quote(ruta)
+        r1 = session.get(primera_url, allow_redirects=True, timeout=30)
 
-        url = f"{base_url}{download_path}?nombrefichero={ruta_codificada}"
-
-        session = requests.Session()
-        session.headers.update({"User-Agent":"Mozilla/5.0",})
-
-        # 1. Entrar en la URL preotegida para que el servidor nos redirija al login
-        r1 = session.get(url, allow_redirects=True, timeout=30)
-        print("Paso 1:",r1.status_code, r1.url)
-
-        # 2. Construir la URL de login
         login_url = r1.url
-
         payload = {
-            "Nombre": user,
+            "Nombre" : user,
             "password": password,
             "ActualizarPassword": "False",
             "Agenda": "False",
             "Error": "",
             "Login": "Aceptar",
         }
-
         headers = {
-            "Refer": r1.url,
+            "Referer": r1.url,
             "Content-Type": "application/x-www-form-urlencoded",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "User-Agent": "Mozilla/5.0"
         }
 
-        # 3. Enviar login
         r2 = session.post(login_url, data=payload, headers=headers, allow_redirects=True, timeout=30)
-        print("Paso 2:", r2.status_code, r2.url)
 
-        # 4. Intentar la descarga con la sesión autorizada
-        r3 = session.get(url, stream=True, timeout=60)
-        print("Paso 3:", r3.status_code)
-        print("Content-Type:", r3.headers.get("Content-Type"))
-        print("Content-Disposition:", r3.headers.get("Content-Disposition"))
+        for ruta in ruta_list:
+            try:
+                ruta_codificada = quote(ruta) #type:ignore
+                url = f"{base_url}{download_path}?ruta={ruta_codificada}"
 
-        # 5. Comprobar si sigue devolviendo html
-        content_type = (r3.headers.get("Content-Type") or "").lower()
+                r3 = session.get(url, stream=True, timeout=60)
+                print("Descarga: ",r3.status_code, url)
+                content_type = (r3.headers.get("Content-Type") or "").lower()
+                content_disposition = r3.headers.get("Content_Disposition", '"')
+
+                if r3.status_code == 200:
+                    filename = extraer_nombre_fichero(ruta, content_disposition)
+                    print("Guardando como: ",filename)
+                    
+                    filepath = os.path.join(RAW_DIR, filename)
+                    filepath = generar_nombres_unicos(filepath)
+
+                    with open(filepath, "wb") as f:
+                        for chunk in r3.iter_content(chunk_size=8192):
+                            if chunk:
+                                f.write(chunk)
+                    print("Archivo guardado")
+                else:
+                    with open("error_response.html", "w", encoding="utf-8") as f:
+                            f.write(r3.text)
+
+                            print("Descarga erronea, se obtuvo HTML en vez del archivo")
+            except Exception as e:
+                print(f"Error descargando {ruta}: {e}")
+    finally:
+        session.close()
+
+def extract_base64(lines):
+    """
+    Encuentra dónde empieza el contenido base64.
+    Primero busca 'Content-Disposition:'.
+    Si no lo encuentra, empieza desde la línea 5.
+    """
+    start_index = None
+
+    for i, line in enumerate(lines):
+        if "Content-Disposition:" in line:
+            start_index = i + 1
+            break
+
+    if start_index is None:
+        start_index = 5
+
+    base64_data = "".join(lines[start_index:]).strip()
+    return base64_data
 
 
-        if r3.status_code == 200 and "text/html" not in content_type:
-            filename = str(re.search(r'I1084[^\\]+$', ruta_codificada))
+def decode_file(filepath):
+    with open(filepath, "r", encoding="utf-8", errors="ignore") as f:
+        lines = f.readlines()
 
-            #probar capturar el nombre que manda el server
-            content_disposition = r3.headers.get("Content-Disposition", "")
-            if "filename" in content_disposition:
-                raw_filename = content_disposition.split("filename=")[-1].strip().strip('"')
+    base64_data = extract_base64(lines)
 
-                filename = os.path.basename(raw_filename)
-                filename = filename.replace("\\","_")
-            print("Intentando guardar como:", filename)
-            
-            with open(filename, "wb") as f:
-                    for chunk in r3.iter_content(chunk_size=8192):
-                        if chunk:
-                            f.write(chunk)
-            
-            
-            print(f"Archivo guardado como {filename}")
+    try:
+        decoded = base64.b64decode(base64_data)
+        return decoded
+    except Exception as e:
+        print(f"Error decodificando {filepath}: \n{e}")
+        return None
+
+
+def extract_segments(edi_text):
+    """
+    Busca los segmentos requeridos dentro del EDI
+    """
+    results = {}
+
+    patterns = {
+        "BGM+80E Nº Pedido":r"BGM\+80E:[^+]*\+([^+]+)",
+        "DTM+264 FCP Mayor o igual": r"DTM\+264:(\d{8})",
+        "DTM+267 FCP exactamente igual": r"DTM\+267:(\d{8})",
+        "RFF+FCP FCP según porcentaje vida útil": r"RFF\+FCP:([^']+)"
+    }
+
+    for key, pattern in patterns.items():
+        match = re.search(pattern, edi_text)
+        if match:
+            result = match.group(1)
+
+            if "DTM" in key:
+                result = datetime.strptime(result, "%Y%m%d").strftime("%Y-%m-%d")
+
         else:
-            #Guardamos la respuesta
-            with open("error_response.html", "w", encoding="utf-8") as f:
-                f.write(r3.text)
-                print("Se ha guardado la respuesta HTML de error en 'error_response.html'")
-            
-            print("La descarga falló. Se obtuvo una respuesta HTML de error." )
-            print("Se ha guardado la respuesta en 'error_response.html'")
-                
-            
+            result = None
+        results[key] = result
+    return results
+
+
+def process_files(input_folder, output_folder):
+
+    for filename in os.listdir(input_folder):
+
+        filepath = os.path.join(input_folder, filename)
+
+        if not os.path.isfile(filepath):
+            continue
+
+        print(f"Procesando: {filename}")
+
+        decoded = decode_file(filepath)
+
+        if decoded is None:
+            continue
+
+        edi_text = decoded.decode("utf-8", errors="ignore")
+
+        # Guardar EDI
+        edi_name = os.path.splitext(filename)[0] + ".edi"
+        edi_path = os.path.join(output_folder, edi_name)
+
+        with open(edi_path, "w", encoding="utf-8") as f:
+            f.write(edi_text)
+
+        # Extraer segmentos
+        segments = extract_segments(edi_text)
+
+        print("Segmentos encontrados:")
+        for k, v in segments.items():
+            print(f"  {k}: {v}")
+
+        print("-" * 40)                          
+    
