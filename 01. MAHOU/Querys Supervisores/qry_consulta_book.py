26148fd15f7dda2668fd15fd82d5dc237705fcea
Demergxo <javiergmpgxo@gmail.com>
2026-04-22 10:47:00 +0200

    Modificaciones en archivo booking según nuevas especificaciones

---
+++ 01. MAHOU/Querys Supervisores/qry_consulta_book.py
@@ -0,0 +1,177 @@
+from sqlalchemy import create_engine, text, bindparam #type:ignore
+import pandas as pd
+from datetime import datetime
+
+date = datetime.now().strftime("%Y%m%d%H%M%S")
+#ddbb_name = ""
+
+fecha_inicio = '2026-03-09'
+fecha_fin = '2026-03-22'
+
+def hora():
+    hora = datetime.now().strftime("%H:%M:%S")
+    return hora
+
+print(f"Hora de inicio: {hora()}")
+
+# --- CONEXIÓN SQLALCHEMY ---
+
+engine = create_engine("mssql+pyodbc://@XGA_PROD")
+
+# --- QUERY SQL (rango de fechas) ---
+query = text("""
+    SELECT
+        ID_Doc, CodigoTipoDocumento AS [Tipo Documento],AlbaranDoc AS [Pedido MSM],
+        PaletsClienteDoc AS [Palets Estimados], CampoCliente AS [Codigo Doc], PedidoDestinatarioDoc AS [Pedido Cliente]
+    FROM
+        vDocumentos
+    WHERE 
+        ID_Cliente = 944
+        AND CONVERT(date, FechaDoc) BETWEEN CONVERT(date, :inicio) AND CONVERT(date, :fin)
+        AND CodigoTipoDocumento IN ('RS', 'ALB') 
+        AND ID_Almacen = 129
+""")
+
+# --- EJECUTAR CONSULTA ---
+with engine.connect() as conn:
+    df = pd.read_sql(query, conn, params={"inicio": fecha_inicio, "fin": fecha_fin})
+
+#reemplazamos valores
+df["Tipo Documento"] = df["Tipo Documento"].astype(str).str.strip()
+df["Tipo Documento"] = df["Tipo Documento"].map({
+    "ALB": "Cargas",
+    "RS": "Descargas"
+}).fillna(df["Tipo Documento"])
+
+
+df["Pedido MSM"] = df["Pedido MSM"].astype(str).str.strip()
+list_ids = df["Pedido MSM"].tolist()
+df["ID_Doc"] = df["ID_Doc"].astype(str).str.strip()
+list_doc = df["ID_Doc"].tolist()
+
+
+    #Query acotada a ID_Doc necesarios
+if not list_ids:
+    raise ValueError("Lista vacía")
+    
+
+dfs =[]
+
+placeholders = ", ".join([f":id{i}" for i in range(len(list_ids))])
+placeholders2 = ", ".join([f":id{i}" for i in range(len(list_doc))])
+
+query_pend = text(f"""
+    SELECT
+        AlbaranDoc AS [Pedido MSM],
+        FechaTeoricaCargaDoc AS [Fecha Carga],
+        ARCDoc AS [Observaciones]
+    FROM 
+        Documentos
+    WHERE 
+        ID_Cliente = 944
+        AND ID_Almacen = 129
+        AND (ID_TipoDocumento = 1 OR ID_TipoDocumento = 3)
+        AND CONVERT(datetime, FechaDoc) BETWEEN CONVERT(date, :inicio) AND CONVERT(date, :fin)
+        AND AlbaranDoc IN ({placeholders})
+    """)
+
+params_ids = {f"id{i}": v for i, v in enumerate(list_ids)}
+
+
+params = {
+    **params_ids,
+    "inicio": fecha_inicio,
+    "fin": fecha_fin
+}
+
+with engine.connect() as conn:
+    df_palets = pd.read_sql(query_pend, conn, params=params)
+df_palets["Pedido MSM"] = df_palets["Pedido MSM"].str.strip()
+
+
+df = df.merge(df_palets, on="Pedido MSM", how="left")
+df["ID_Doc"] = df["ID_Doc"].astype(str).str.strip()
+
+query_lineas_in = text("""
+    SELECT
+        AlbaranDoc AS [Pedido MSM],
+        CodigoProdClte AS Referencia,        
+        SUM(CantidadLinea) AS Palets
+    FROM
+        vLineasOrdenCompraAlbaranDoc
+    WHERE
+        ID_Cliente = 944
+        AND AlbaranDoc IN :ids
+    GROUP BY
+        AlbaranDoc, CodigoProdClte
+""").bindparams(
+    bindparam("ids", expanding=True))
+
+query_lineas_out = text(f"""
+    SELECT
+        RTRIM(ID_Doc) AS ID_Doc,
+        CodigoProdClte AS Referencia,        
+        SUM(CantidadTeorica) AS CantidadTeorica
+    FROM
+        vDocumentoslineasConsulta
+    WHERE
+        ID_Cliente = 944
+        AND ID_Doc IN ({placeholders2})
+    GROUP BY RTRIM(ID_Doc), CodigoProdClte, NombreProdClte                                  
+""")
+
+
+params_docs = {f"id{i}": v for i, v in enumerate(list_doc)}
+
+#segunda tanda de consultas
+with engine.connect() as conn:
+    df_lineas_in = pd.read_sql(query_lineas_in, conn, params={"ids": list_ids}) #type:ignore
+    df_lineas_out = pd.read_sql(query_lineas_out, conn, params=params_docs)
+df_lineas_in["Pedido MSM"] = df_lineas_in["Pedido MSM"].str.strip()
+
+
+#unimos y eliminamos
+df = df.merge(df_lineas_in, on="Pedido MSM", how="left")
+df = df.merge(df_lineas_out, left_on="ID_Doc", right_on="ID_Doc", how="left")
+
+df = df.drop(columns=["ID_Doc"])
+
+#formateamos el dataframe
+df["Referencia"] = df["Referencia_x"].combine_first(df["Referencia_y"])
+df["Palets"] = df["Palets"].combine_first(df["CantidadTeorica"])
+df = df.drop(columns=["Referencia_x", "Referencia_y", "CantidadTeorica"])
+df["Referencia"] = df["Referencia"].fillna("").astype(str)
+df["Referencia"] = df["Referencia"].str.strip().str.zfill(3)
+
+#eliminamos 004->palet chep y 012->medio display
+df = df[~df["Referencia"].isin(["004", "012"])]
+
+# Dividimos por fecha y hora
+df["Fecha"] = pd.to_datetime(df["Fecha Carga"]).dt.strftime("%Y-%m-%d")
+df["Hora"] = pd.to_datetime(df["Fecha Carga"]).dt.strftime("%H:%M:%S")
+
+#unimos referencias en un solo campo y sumamos palets
+df_final = df.groupby("Pedido MSM", as_index=False).agg({
+    "Fecha": "first",
+    "Hora": "first",
+    "Referencia": lambda x: "-".join(sorted(set(i for i in x if i))),
+    "Palets": "sum",
+    "Codigo Doc": "first",
+    "Observaciones": "first",
+    "Tipo Documento": "first",
+    "Pedido Cliente": "first",
+    "Palets Estimados": "first"
+    
+})
+
+# --- EXPORTAR ---
+nombre_archivo = f"booking_{date}.xlsx"
+df_final.to_excel(nombre_archivo, index=False)
+
+print(f"✅ Archivo generado correctamente: {nombre_archivo}")
+
+print(f"Hora de fin: {hora()}")
+engine.dispose()
+
+
+
